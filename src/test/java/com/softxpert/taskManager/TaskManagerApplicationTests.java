bb9d350ea72e87f73ee1afae3d8a931224003de0
zeyad.khalil <zeyad.khalil@3ddx.com>
2025-11-22 14:03:01 +0200

    initializing task manager project, and linking it to mySql DB

---
+++ src/test/java/com/softxpert/taskManager/TaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softxpert.taskManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
